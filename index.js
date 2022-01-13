fa48298f72e42e5c74fb914d77f76bf999f62457
Anthony Coman <acoman@yahoo.com>
2022-01-13 11:46:59 -0600

    chugging along

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log(process.argv);
+const user = process.env.USERNAME;
+
+if (process.argv[2]==='web-49'){
+console.log('web 49 is plummenting headfirst into the ground')
+}else{
+	console.log('booo!')
+}
+
+console.log(`the user is ${user}`)

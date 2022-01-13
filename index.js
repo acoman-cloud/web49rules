74f67d87cc6fdcaa485f4a3089835cfe23a274f7
Anthony Coman <acoman@yahoo.com>
2022-01-13 11:51:35 -0600

    wip

--- index.js
+++ index.js
@@ -8,3 +8,15 @@ console.log('web 49 is plummenting headfirst into the ground')
 }
 
 console.log(`the user is ${user}`)
+
+const express = require('express')
+const app = express()
+
+app.get('/hello', (req, res) => {
+  res.json({ message: 'hey there'})
+})
+
+const port = 9000;
+app.listen(port, ()=>{
+	console.log(`listening on port ${port}`)
+})
